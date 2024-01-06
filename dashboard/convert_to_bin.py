2d0322d53ca6c6d0ddb4acf03eabbe0221e09978
Vojtěch Lukáš <63453314+voytex@users.noreply.github.com>
2024-01-06 15:00:38 +0100

    added http preamble

--- dashboard/convert_to_bin.py
+++ dashboard/convert_to_bin.py
@@ -7,10 +7,13 @@ if len(sys.argv) < 2:
 file_to_convert = sys.argv[1]
 buffer          = ""
 result_file     = file_to_convert.replace(".html", ".bin")
+preamble =  "HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: close\n\n"
 
 buffer = open(file_to_convert, "r").read()
 
 with open(result_file, "wb") as bin_file:
+    for c in preamble:
+        bin_file.write(bytes(c, 'utf-8'))
     for c in buffer:
         if c not in ['\n', '\t', '\r']:
             bin_file.write(bytes(c, 'utf-8'))
